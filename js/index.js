b1f3146e10ee4b22206db5647227a4cb1e3dc2c8
jungmtg <cardofang@gmail.com>
2017-10-02 17:56:18 +0800

    test jsyaml

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    getYaml()
+});
+
+function getYaml(){
+    $.get('./test.yaml', function(res){
+        var obj = jsyaml.load(res);
+        console.log(obj)
+        genElm(obj)
+    })
+}
+
+function genElm(obj){
+    var html = '<ul>'
+    for (var i = 0; i < obj.length; i++) {
+        var el = obj[i];
+        html+= '<li>'+el+'</li>'
+    }
+    html += '</ul>'
+    console.log(html)
+    $("#main").append(html)
+}
