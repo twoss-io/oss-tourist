dcda4524fd80ac6680eb9b595594fd49418567be
jungmtg <cardofang@gmail.com>
2017-10-03 12:18:10 +0800

    fix

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    getYaml()
+});
+
+function getYaml(){
+    $.get('./category.yaml', function(res){
+        var obj = jsyaml.load(res);
+        console.log(obj)
+        genElm(obj)
+    })
+}
+
+function genElm(obj){
+    var html = '<ul id="navList">'
+    for (var i = 0; i < obj.length; i++) {
+        var el = obj[i];
+        html+= '<li>'+el+'</li>'
+    }
+    html += '</ul>'
+    console.log(html)
+    $("#main").append(html)
+    $("#navList").addClass("sidebar-nav");
+    $("#main").addClass("sidebar-toggle");
+}
