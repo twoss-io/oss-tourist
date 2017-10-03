63cdc23c9fbbfd9baeaa495c4ff9dc5e886b95b9
jungmtg <cardofang@gmail.com>
2017-10-03 17:18:01 +0800

    fix

--- vendor/jquery/index.js
+++ vendor/jquery/index.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 });
 
 function getYaml(){
-    $.get('../../category.yaml', function(res){
+    $.get('./category.yaml', function(res){
         var obj = jsyaml.load(res);
         console.log(obj)
         genElm(obj)
