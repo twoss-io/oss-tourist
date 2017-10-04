bfe16e3c5f8d7b808d5815674e96bd63c2dfbe47
jungmtg <cardofang@gmail.com>
2017-10-04 18:03:13 +0800

    add Video Slide Blog yaml file per category and add javascript function to read yaml file

--- vendor/jquery/index.js
+++ vendor/jquery/index.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 function getYaml(){
     $.get('./category.yaml', function(res){
         var obj = jsyaml.load(res);
-        console.log(obj)
+        //console.log(obj)
         genElm(obj)
     }).done(function(){
         console.log("done")
@@ -22,12 +22,60 @@ function genElm(obj){
     var html = '<ul id="navList">'
     for (var i = 0; i < obj.length; i++) {
         var el = obj[i];
-        //html+= '<li><a href="#">'+el+'</a></li>'
-        html+= '<li>'+el+'</li>'
+        html+= '<li><a href="'+'#'+'" onclick="getCategory('+'\''+el+'\''+');">'+el+'</a></li>'
+        //html+= '<li>'+el+'</li>'
     }
     html += '</ul>'
-    console.log(html)
     //$("#main").append(html)
     $("#sidebar-wrapper").append(html)
     
+}
+
+function getCategory(category){
+    console.log(category);
+    getVideoByCategory(category);
+    getSlideByCategory(category);
+    getBlogByCategory(category);
+}
+
+function getVideoByCategory(category){
+    //Todo
+    console.log('start in getVideoByCategory:'+category);
+    $.get('./'+category+'_Video.yaml', function(res){
+        var obj = jsyaml.load(res);
+        console.log(obj)
+        //genElm(obj)
+    }).done(function(){
+        console.log("Video done")
+    }).fail(function() {
+        alert(category +'video is not completed,please add or update '+category+'_Video.md');
+        console.log( "Video fail" );
+      })
+}
+function getSlideByCategory(category){
+    console.log('start in getSlideByCategory:'+category);
+    $.get('./'+category+'_Slide.yaml', function(res){
+        var obj = jsyaml.load(res);
+        console.log(obj)
+        //genElm(obj)
+    }).done(function(){
+        console.log("Slide done")
+    }).fail(function() {
+        alert(category +'Slide is not completed,please add or update '+category+'_Slide.md');
+        console.log( "Slide fail" );
+      })
+}
+function getBlogByCategory(category){
+    //Todo
+    console.log('start in getBlogByCategory:'+category);
+    $.get('./'+category+'_Blog.yaml', function(res){
+        var obj = jsyaml.load(res);
+        console.log(obj)
+        //genElm(obj)
+    }).done(function(){
+        console.log("Blog done")
+    }).fail(function() {
+        alert(category +'Blog is not completed,please add or update '+category+'_Blog.md');
+        console.log( "Blog fail" );
+      })
 }
