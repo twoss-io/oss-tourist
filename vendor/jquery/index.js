c437c813c28887d407f731bf75456a7ac3c09213
andy84425 <33613860+andy84425@users.noreply.github.com>
2017-11-27 13:38:21 +0800

    Add files via upload

--- vendor/jquery/index.js
+++ vendor/jquery/index.js
@@ -93,7 +93,7 @@ function getVideoByCategory(category) {
     $.get('./content/' + category + '.yaml', function (res) {
         var obj = jsyaml.load(res);
         console.log(obj);
-        genVideoElm(obj);
+        genVideoElm(obj, 1, 6, category);
         //Get Yutube image and Set to iframe
         $(function () {
             var thumbSize = 'large',		// 設定要取得的縮圖是大圖還是小圖
@@ -146,7 +146,7 @@ function getSlideByCategory(category) {
     $.get('./content/' + category + '.yaml', function (res) {
         var obj = jsyaml.load(res);
         //console.log(obj);
-        genSlideElm(obj);
+        genSlideElm(obj, 1, 6, category);
         //genElm(obj)
     }).done(function () {
         console.log("Slide done")
@@ -162,7 +162,7 @@ function getBlogByCategory(category) {
     $.get('./content/' + category + '.yaml', function (res) {
         var obj = jsyaml.load(res);
         //console.log(obj);
-        genBlogElm(obj);
+        genBlogElm(obj, 1, 6, category);
         console.log('genBlogElm finished');
         $('img[data-url]').each(function() {
             console.log('start traversal in img[data-url]')
@@ -188,16 +188,22 @@ function getBlogByCategory(category) {
 }
 
 
-function genBlogElm(obj) {
+function genBlogElm(obj, nowPage, perNum, category) {
     var dataSet = [];
     var html = ''//'<h4>Blog</h4>'//'<tr>'
     var b = 0;
     var exist = false ;
-    html += '<h1 id = "blog">Blog</h1>'
+    var begin = ( nowPage - 1 ) * perNum ; // 根據頁數之起始數 e.g 第一頁0 第二頁6個   
+    var end = nowPage * perNum ; // 根據頁數之結束數 e.g 第一頁6 第二頁12個   
+    var nowPassNum = 0; // 已經找到之其類別的個數
+    //console.log('genBlogElm obj:'+obj);
     //console.log('genBlogElm obj:'+obj);
     for (var i in obj) {
         if (String(obj[i].dpl) == 'b') {
             exist = true ;
+            nowPassNum++ ; // 經過的類別個數
+            if ( nowPassNum < begin + 1 || nowPassNum > end ) continue ; // 只秀出目標範圍的東西
+            
             var object1 = [obj[i].ttl, obj[i].url];
            
             // html += '<div class="col-md-4">'
@@ -233,23 +239,57 @@ function genBlogElm(obj) {
            
         }
     }
+    
+    
+      var totalPage = Math.ceil(nowPassNum / perNum) ;
+      
+      var before, after ;
+      if ( nowPage > 1 ) before = nowPage - 1 ;
+      else before = 1 ;
+      
+      if ( nowPage < totalPage ) after = nowPage + 1 ;
+      else after = totalPage ;
+    
+      html += '<div id = "blogPagePicker" class = "col-sm-12">' 
+      html += '<div style = "width:80%;padding:0px 0px 10px; margin:auto;">'
+      html +=   '<span style= "padding:0px 8px">Blog Page '+ nowPage + '/' + totalPage +'</span>' ;
+      html +=   '<button onclick="changePage( '+ before +', \'Blog\', \'' + category + '\' )" style = "display:inline-block;" type="button" class="btn btn-default btn-sm">';
+      html +=     '<span class="glyphicon glyphicon-arrow-left"></span> Left';
+      html +=   '</button>';
+      html +=   '<button onclick="changePage( '+ after +', \'Blog\', \'' + category + '\' )" style = "display:inline-block;" type="button" class="btn btn-default btn-sm">';
+      html +=     '<span class="glyphicon glyphicon-arrow-right"></span> Right';
+      html +=   '</button>';
+      html +=   '<div style = "margin:auto ; padding: 0px 30px ; display:inline-block;">' ;
+      for ( var h = 1 ; h <= totalPage ; h++ ) {
+        html +=   '<button onclick="changePage( '+ h +', \'Blog\', \'' + category + '\' )" style = "display:inline-block;" type="button" class="btn btn-default btn-sm">'+ h +'</button>';
+      } // for
+      html +=   '</div>' ;
+      html += '</div>' 
+      html += '</div>' 
+    
+    
     //html+='</tr>'
     if ( exist == true ) $("#blogbody").append(html)
 
     
 }
 
-function genSlideElm(obj) {
+function genSlideElm(obj, nowPage, perNum, category) {
     var html = ''//'<h4>Slide</h4>'//'<tr>'
     var s = 0;
     var exist = false ;
-    html += '<h1 id = "slide">Slide</h1>'
+    var begin = ( nowPage - 1 ) * perNum ; // 根據頁數之起始數 e.g 第一頁0 第二頁6個   
+    var end = nowPage * perNum ; // 根據頁數之結束數 e.g 第一頁6 第二頁12個   
+    var nowPassNum = 0; // 已經找到之其類別的個數
     //console.log('genBlogElm obj:'+obj);
     for (var i in obj) {
         var desvalue = '';
         var ttlvalue = '';
         if (String(obj[i].dpl) == 's') {
             exist = true ;
+            nowPassNum++ ; // 經過的類別個數
+            if ( nowPassNum < begin + 1 || nowPassNum > end ) continue ; // 只秀出目標範圍的東西
+
             //if (String(obj[i].des) == null) { obj[i].des = ''; }
 
             if (typeof obj[i].des === 'undefined' || obj[i].des === null) {
@@ -296,23 +336,56 @@ function genSlideElm(obj) {
         
         
     }
+    
+    
+      var totalPage = Math.ceil(nowPassNum / perNum) ;
+      
+      var before, after ;
+      if ( nowPage > 1 ) before = nowPage - 1 ;
+      else before = 1 ;
+      
+      if ( nowPage < totalPage ) after = nowPage + 1 ;
+      else after = totalPage ;
+    
+      html += '<div id = "slidePagePicker" class = "col-sm-12">' 
+      html += '<div style = "width:80%;padding:0px 0px 10px; margin:auto;">'
+      html +=   '<span style= "padding:0px 8px">Slide Page '+ nowPage + '/' + totalPage +'</span>' ;
+      html +=   '<button onclick="changePage( '+ before +', \'Slide\', \'' + category + '\' )" style = "display:inline-block;" type="button" class="btn btn-default btn-sm">';
+      html +=     '<span class="glyphicon glyphicon-arrow-left"></span> Left';
+      html +=   '</button>';
+      html +=   '<button onclick="changePage( '+ after +', \'Slide\', \'' + category + '\' )" style = "display:inline-block;" type="button" class="btn btn-default btn-sm">';
+      html +=     '<span class="glyphicon glyphicon-arrow-right"></span> Right';
+      html +=   '</button>';
+      html +=   '<div style = "margin:auto ; padding: 0px 30px ; display:inline-block;">' ;
+      for ( var h = 1 ; h <= totalPage ; h++ ) {
+        html +=   '<button onclick="changePage( '+ h +', \'Slide\', \'' + category + '\' )" style = "display:inline-block;" type="button" class="btn btn-default btn-sm">'+ h +'</button>';
+      } // for
+      html +=   '</div>' ;
+      html += '</div>' 
+      html += '</div>' 
+    
+    
     html += ''//'</tr>'
     if ( exist == true ) $("#slidebody").append(html)
 
 }
 
-
-function genVideoElm(obj) {
+function genVideoElm(obj, nowPage, perNum, category) {
     var html = ''//'<h4>Video</h4>'//'<tr align="left">'
     var v1 = 0;
     var exist = false ;
-    html += '<h1 id = "video">Video</h1>'
+    var begin = ( nowPage - 1 ) * perNum ; // 根據頁數之起始數 e.g 第一頁0 第二頁6個   
+    var end = nowPage * perNum ; // 根據頁數之結束數 e.g 第一頁6 第二頁12個   
+    var nowPassNum = 0; // 已經找到之其類別的個數
     //console.log('genBlogElm obj:'+obj);
-    for (var i in obj) {
+    for ( var i in obj) {
         var desvalue = '';
         var ttlvalue = '';
         if (String(obj[i].dpl) == 'v') {
             exist = true ;
+            nowPassNum++ ; // 經過的類別個數
+            if ( nowPassNum < begin + 1 || nowPassNum > end ) continue ; // 只秀出目標範圍的東西
+              
             if (typeof obj[i].des === 'undefined' || obj[i].des === null) {
 
             } else {
@@ -329,8 +402,6 @@ function genVideoElm(obj) {
             } else {
                 ttlvalue = String(obj[i].ttl);
             }
-            
-            
             html += '<div class="col-xs6 col-sm-4 ">'
             html += '<div class="panel">'
             html += '<div class="panel-heading">'
@@ -351,10 +422,38 @@ function genVideoElm(obj) {
             html += '</div>'
             html += '</div>'
             
+            
         }
+        
     }
+    
+      var totalPage = Math.ceil(nowPassNum / perNum) ;
+      
+      var before, after ;
+      if ( nowPage > 1 ) before = nowPage - 1 ;
+      else before = 1 ;
+      
+      if ( nowPage < totalPage ) after = nowPage + 1 ;
+      else after = totalPage ;
+    
+      html += '<div id = "videoPagePicker" class = "col-sm-12">' 
+      html += '<div style = "width:80%;padding:0px 0px 10px; margin:auto;">'
+      html +=   '<span style= "padding:0px 8px">Video Page '+ nowPage + '/' + totalPage +'</span>' ;
+      html +=   '<button onclick="changePage( '+ before +', \'Video\', \'' + category + '\' )" style = "display:inline-block;" type="button" class="btn btn-default btn-sm">';
+      html +=     '<span class="glyphicon glyphicon-arrow-left"></span> Left';
+      html +=   '</button>';
+      html +=   '<button onclick="changePage( '+ after +', \'Video\', \'' + category + '\' )" style = "display:inline-block;" type="button" class="btn btn-default btn-sm">';
+      html +=     '<span class="glyphicon glyphicon-arrow-right"></span> Right';
+      html +=   '</button>';
+      html +=   '<div style = "margin:auto ; padding: 0px 30px ; display:inline-block;">' ;
+      for ( var h = 1 ; h <= totalPage ; h++ ) {
+        html +=   '<button onclick="changePage( '+ h +', \'Video\', \'' + category + '\' )" style = "display:inline-block;" type="button" class="btn btn-default btn-sm">'+ h +'</button>';
+      } // for
+      html +=   '</div>' ;
+      html += '</div>' 
+      html += '</div>' 
     //html+='</tr>'
-    if ( exist == true ) $("#videobody").append(html)
+    if( exist == true ) $("#videobody").append(html)
     //$('#escalation').paging({limit:1});
     //console.log('videobody='+html);  
 }
@@ -379,8 +478,100 @@ function adjustIframes() {
 }
 $(window).on('resize load', adjustIframes);
 
-function changeScrollbarPosition( targetName ){
-  var loc = $(targetName).offset().top ;
-  $('html, body').animate({scrollTop:(loc-80)},1000);
 
+function image_creator(url, ptitle)
+{
+    $.ajax(
+        { 
+        url: url, 
+        success: function(data) {
+            var html = $.parseHTML( data ), 
+                img = $(html).find("img"),
+                len = img.length; 
+            if( len > 0 ){
+                var src = img.first().attr("src"); // get id of first image
+            } else {
+                console.log("Image not found");
+            }
+            console.log(src);
+
+            image_tag='<img src="'+src+'" alt="'+ptitle+'"/>';
+            return image_tag;
+        },
+        error: function(result){   //處理回傳錯誤事件，當請求失敗後此事件會被呼叫
+          //your code here
+           console.log("Image not found can not connect url ");
+        }
+    });
 }
+
+
+function changePage( targetPageNum, targetType, category ) {
+  $.get('./content/' + category + '.yaml', function (res) {
+    var dataSet = jsyaml.load(res);
+    if( targetType == 'Video' ) {
+      $("#videobody").empty() ;
+      genVideoElm(dataSet, targetPageNum, 6, category) ;
+      $(function () {
+            var thumbSize = 'large',		// 設定要取得的縮圖是大圖還是小圖
+                // 大圖寬高為 480X360；小圖寬高為 120X90
+                //imgWidth = '300',			// 限制圖片的寬及 YouTube 影片的寬
+                imgWidth = '100%',
+                imgHeight = '100%',			// 限制圖片的高及 YouTube 影片的高
+                autoPlay = '&autoplay=1',	// 是否載入 YouTube 影片後自動播放；若不要自動播放則設成 0
+                fullScreen = '&fs=1';		// 是否允許播放 YouTube 影片時能全螢幕播放
+
+            //$('ul.playlist>li>a').each(function () {
+            // $('ul.playlist>a').each(function () {
+            $('p>a').each(function () {
+                //$('ul.playlist>table>tr>td>li>a').each(function(){
+                // 取得要連結轉換的網址及訊息內容
+                var _this = $(this),
+                    _url = _this.attr('href'),
+                    _info = _this.text(),
+                    _type = (thumbSize == 'large') ? 0 : 2;
+                //console.log('_type='+_type);  
+                // 取得 vid
+                var vid = _url.match('[\\?&]v=([^&#]*)')[1];
+                //console.log('vid='+vid);  
+                // 取得縮圖
+                var thumbUrl = "https://img.youtube.com/vi/" + vid + "/" + _type + ".jpg";
+
+                // 把目前超連結的內容轉換成圖片並加入 click 事件
+                _this.html('<img src="' + thumbUrl + '" alt="' + _info + '" title="' + _info + '" width="' + imgWidth + '" height="' + imgHeight + '" />').click(function () {
+                    return false;
+                }).focus(function () {
+                    this.blur();
+                }).children('img').click(function () {
+                    
+                    window.open(_url, '_blank');
+                   
+                });
+            });
+        });
+    } // if
+    else if( targetType == 'Slide' ) {
+      $("#slidebody").empty() ;
+      genSlideElm(dataSet, targetPageNum, 6, category) ;
+    } // else if 
+    else if( targetType == 'Blog' ) {
+      $("#blogbody").empty() ;
+      genBlogElm(dataSet, targetPageNum, 6, category) ;
+      $('img[data-url]').each(function() {
+          console.log('start traversal in img[data-url]')
+          $.ajax({
+            url: 'https://www.googleapis.com/pagespeedonline/v1/runPagespeed?url=' + $(this).data('url') + '&screenshot=true',
+            context: this,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                console.log('start traversal in img[data-url]');
+                 data = data.screenshot.data.replace(/_/g, '/').replace(/-/g, '+');
+                $(this).attr('src', 'data:image/jpeg;base64,' + data);
+              }
+          });
+      });
+    } // else if 
+  })// getCatYaml
+  
+} // changeVideoPage()
